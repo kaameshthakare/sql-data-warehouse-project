9a453844643c5e7639cd811d17e02290b43853b0
kaameshthakare <kameshthakare1234@gmail.com>
2025-11-23 21:34:20 +0530

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -36,7 +36,7 @@ LEFT JOIN silver.erp_cust_az12 ca
 ON   ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
 ON   ci.cst_key = la.cid
-
+GO
 --===========================================================================
 --Create Dimention: gold.dim_product
 --===========================================================================
@@ -60,7 +60,7 @@ FROM silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
 ON pn.cat_id = pc.id
 WHERE prd_end_dt IS NULL  --Filter out allhistorical data
-
+GO
 --===========================================================================
 --Create fact: gold.fact_sales
 --===========================================================================
@@ -84,3 +84,4 @@ LEFT JOIN gold.dim_product pr
 ON sd.sls_prd_key = pr.product_number
 LEFT JOIN  gold.dim_customers cu
 ON sd.sls_cust_id = cu.customer_id
+GO
